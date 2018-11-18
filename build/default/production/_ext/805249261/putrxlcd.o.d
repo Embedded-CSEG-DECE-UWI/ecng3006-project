8014876bd20af8fbd772b0ad84284dc93dadef48
Daniel Gordon <danny.b.gordon@gmail.com>
2018-11-18 02:45:00 -0400

    Added Temp Sensor Files from grpF's prev. ext repo

---
+++ build/default/production/_ext/805249261/putrxlcd.o.d
@@ -0,0 +1,4 @@
+build/default/production/_ext/805249261/putrxlcd.o:  \
+	C:/ECNG3006/Group_Project/GroupFProject/putrxlcd.c  \
+	C:/ECNG3006/Group_Project/GroupFProject/xlcd.h  \
+
---
+++ build/default/production/_ext/805249261/putsxlcd.o.d
@@ -0,0 +1,4 @@
+build/default/production/_ext/805249261/putsxlcd.o:  \
+	C:/ECNG3006/Group_Project/GroupFProject/putsxlcd.c  \
+	C:/ECNG3006/Group_Project/GroupFProject/xlcd.h  \
+

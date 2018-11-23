5b6138ea4af4c3dfb2fbba0d3b34f597c848e4da
Daniel Gordon <daniel.gordon@my.uwi.edu>
2018-11-22 20:54:46 -0400

    Added Jabari L. Code for the WDT:

--- Source/7.WDT.c
+++ Source/7.WDT.c
@@ -1,15 +1,62 @@
 #include <p18cxxx.h>
+#include <reset.h> 
+#include <delays.h>
 
-/* Set configuration bits for use with PICKit3 */
-#pragma config FOSC = HS
-#pragma config WDT = OFF
+#pragma config OSC = XT
+#pragma config WDT = ON
 #pragma config LVP = OFF
+#pragma config WDTPS = 64
 
-//The system should tolerate a power/brown-out reset without corrupting information/display
 
-void main(void)
+//#define WDT_ENABLED
+int count;
+void Dummy_Stuck(void)
 {
-    //Insert Your Peripheral Specific Code here for testing
-    //SLEEP();
+    while(1)
+    {
+        PORTBbits.RB3 = 1;
+        Delay10KTCYx(5);      
+        PORTBbits.RB3 = 0;
+        Delay10KTCYx(5);
+    }                   /* wait till watchdog timeout and then reset system */
+}
+
+void main(void) {
+    
+    //Delay10KTCYx(10);
+    count = 0;
+    TRISB = 0x00;
+    
+    if(!RCONbits.TO)
+    {
+        
+        Delay10KTCYx(10);
+        PORTBbits.RB0 = 1;
+        Delay10KTCYx(10);
+        PORTBbits.RB0 = 0;
+        Delay10KTCYx(10);
+        /* Clear Watchdog Timer */
+        
+        
+    }
+    
+    //ClrWdt();
+    while(1)
+    {
+        PORTBbits.RB4 = 1;
+        Delay10KTCYx(5);      
+        PORTBbits.RB4 = 0;
+        Delay10KTCYx(5);
+        ClrWdt();
+        count++;
+        if(count>10)
+        {
+            Dummy_Stuck();
+        }/* Dummy Stuck loop function */
+    }
+    
+    
+    
+    
     Sleep();
 }

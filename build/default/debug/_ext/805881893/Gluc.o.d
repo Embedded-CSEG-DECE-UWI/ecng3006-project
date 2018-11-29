f7ad59a901df15dd5a56f4b03df8797480bf5bc7
Daniel Gladstone <daniel.gladstone@gmail.com>
2018-11-29 19:14:48 -0400

    Integrated the glucose module into the final demo source code. Some formatting changes need to be done before wiki upload

---
+++ build/default/debug/_ext/805881893/Gluc.o.d
@@ -0,0 +1,3 @@
+build/default/debug/_ext/805881893/Gluc.o:  \
+	C:/ECNG3006/Group_Project/ecng3006-project/Source/Gluc.c  \
+
---
+++ build/default/production/_ext/805881893/Gluc.o.d
@@ -0,0 +1,3 @@
+build/default/production/_ext/805881893/Gluc.o:  \
+	C:/ECNG3006/Group_Project/ecng3006-project/Source/Gluc.c  \
+

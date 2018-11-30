9ba806e5030d6415f43e9b0bb8d7596f89398ecc
NicholasVSingh <33099433+NicholasVSingh@users.noreply.github.com>
2018-11-30 19:31:07 -0400

    Add files via upload

---
+++ Speaker.c
@@ -0,0 +1,31 @@
+#include <p18f452.h>
+#include <delays.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pwm.h>
+#include <timers.h>
+
+#pragma config WDT = OFF //Disable watchdog timer
+#pragma config LVP = OFF //Disable low voltage programming
+
+#define _XTAL_FREQ 4000000UL
+
+void main(void)
+{
+    // Set RC2 as output for CCP1 
+    TRISCbits.TRISC2 = 0;
+    
+    // Set duty cycle
+    // aka volume of speaker
+    // 10 bit , 2^10 = 1024 
+    SetDCPWM1(500);  // 50%
+    
+    OpenTimer2(TIMER_INT_OFF & T2_PS_1_4 & T2_POST_1_1);
+    
+    // Period of PWM
+    // Sets frequency or tone
+    OpenPWM1(0xF9);  // 1ms
+    
+    return;
+}

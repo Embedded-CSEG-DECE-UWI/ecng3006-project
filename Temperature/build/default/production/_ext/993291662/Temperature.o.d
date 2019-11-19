a9cde2d0b17c2662cffcf09d4c0d643f53e7a21c
Amar Pustam <Amar Pustam@LAPTOP-G177O5EI>
2019-11-19 11:37:10 -0400

    Initial temperature sensor commit

--- Temperature/build/default/production/_ext/993291662/Temperature.o.d
+++ Temperature/build/default/production/_ext/993291662/Temperature.o.d
@@ -1,5 +1,5 @@
-build/default/production/_ext/993291662/lab1lcd.o:  \
-	F:/ECNG3006/Group_Project/Temperature/lab1lcd.c  \
+build/default/production/_ext/993291662/Temperature.o:  \
+	F:/ECNG3006/Group_Project/Temperature/Temperature.c  \
 	F:/ECNG3006/Group_Project/Temperature/delays.h  \
 	F:/ECNG3006/Group_Project/Temperature/xlcd.h  \
 	F:/ECNG3006/Group_Project/Temperature/ow.h  \

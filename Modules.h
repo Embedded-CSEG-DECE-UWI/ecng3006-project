916289f07682c3f3b8c95b8c5d3715e16ab39c55
Yehu Carrington <yehu41@gmail.com>
2019-11-28 16:11:17 -0400

    uploads The demonstration code of the temperature Sensor
    This commit is a freeRTOS implementation of the Temperature Sensor Demo
    The code creates 2 tasks and a queue that shares information between the Task,
    The first task does the temperature sensor communication and creates a string which it
    sends it to a queue.
    The second task blocks on a queue indefinitely and when it sees data it prints out to the screen
    depending on the ID of the data that is present.

--- Modules.h
+++ Modules.h
@@ -2,65 +2,27 @@
  * File:   Modules.h
  * Author: 815008434
  *
- * Created on November 12, 2019, 3:53 PM
+ * Created on November 11, 2019, 7:01 PM
  */
 
 #ifndef MODULES_H
 #define	MODULES_H
 
-/**********KEYPAD MODULE**********/
+#include    <p18f452.h>
+#include    <delays.h>
+#include    "FreeRTOS.h"
+#include    <semphr.h>
+#include    <queue.h>
 
-/*
- * Key Press cases.
- * (key)(4bit#) -   Function
- * (1)(0000)  -   Select
- * (2)(0001)  -   
- * (3)(0010)  -   
- * (A)(0011)  -   
- * (4)(0100)  -   
- * (5)(0101)  -   
- * (6)(0110)  -   
- * (B)(0111)  -   
- * (7)(1000)  -   
- * (8)(1001)  -   
- * (9)(1010)  -  
- * (C)(1011)  -
- * (0)(1100)  -   
- * (F)(1101)  -
- * (E)(1110)  -
- * (D)(1111)  -
- */
+#define USEC18DELAY 0
+#define USEFREERTOSQUEUE 1
 
-/*CONFIGURATION*/
+/*****GLOBAL SEMAPHORE/QUEUE VARIABLES*****/
 
-#define BIT0    PORTCbits.RC4 
-#define BIT1    PORTCbits.RC5
-#define BIT2    PORTCbits.RC6
-#define BIT3    PORTCbits.RC7
+extern QueueHandle_t xlcdQueue;
 
-/*VARIABLE DECLARATIONS*/
+/**********TEMPERATURE SENSOR MODULE**********/
 
-/*
- * configKeypad
- * 
- * configures the keypad by enabling the RB0 interrupt
- * and setting the TRISC upper nibble as inputs
- */
-void configKeypad(void);
-
-void KeyPressInterrupt(void);
-
-/******************HEART RATE MODULE********************/
-void heartRateModule(void);
-void timer10sIsr(void);
-void bpmCountIsr(void);
-//void hrVarTmrIsr(void);
-
-/***********************SPEAKER MODULE**************************/
-void initSpeakerModule(void);
-void bpmAlert(void);
-
-/***********************TEMPRATURE MODULE***********************/
 #define OW_LAT LATBbits.LATB4
 #define OW_PIN PORTBbits.RB4
 #define OW_TRIS TRISBbits.TRISB4
@@ -68,15 +30,22 @@ void bpmAlert(void);
 #define OUTPUT 0
 #define INPUT 1
 
-unsigned char initSequence(void);
-void writeByte(unsigned char data);
-char readByte(void);
+unsigned char ow_reset(void);
 
-/***************************BOR Modules*************************/
-void LVDisr (void);
-void configBOR (void);
+void ow_write_byte(unsigned char data);
 
+char ow_read_byte(void);
 
+void tempTask (void *pvParameter);
+
+/********LCD MODULE*********/
+
+void lcdTask (void *pvParameter);
+
+struct lcdMessage{
+    unsigned char srcID;
+    unsigned char srcMessage[16];
+};
 
 #endif	/* MODULES_H */
 
--- main.c
+++ main.c
@@ -1,260 +1,45 @@
-#include    <p18f452.h>
-#include    <stdio.h>
-#include    <stdlib.h>
-#include    "Modules.h"
+/* 
+ * File:   main.c
+ * Author: Yehu Carrington
+ *
+ * Created on November 25, 2019, 11:25 AM
+ */
+
+/*HEADER FILES*/
+#include    "FreeRTOS.h"
+#include    "task.h"
+#include    "queue.h"
 #include    "xlcd_GpE.h"
-#include    <delays.h>
+#include    "timers.h"
+#include    "Modules.h"
+#include    <p18f452.h>
 
-
+/*PIC18 CONFIGURATION SETTINGS*/
 #pragma config OSC = HS
 #pragma config WDT = OFF
 #pragma config LVP = OFF
 #define XTAL_FREQ 4000000
 
-//Global Variables
-int key;                //KeyPad GV
-
-// Brown Out configuration
-#pragma config BOR = ON         // Brown-out Reset Enable bit (Brown-out Reset enabled)
-#pragma config BORV = 42        // Brown-out Reset Voltage bits (VBOR set to 4.2V)
-
-/************************GLOBAL VARIABLES FOR TEMPRATURE SENSOR***********************/
-unsigned char tempLSB;
-unsigned char tempMSB;
-unsigned int tempVarMSB = 0;
-unsigned int tempVarLSB = 0;
-unsigned int intTemp = 0;
-unsigned char charTemp;
-unsigned char fractChar;
-unsigned int fractInt = 0;
-unsigned char dispVal[16];
-unsigned char degChar = 0xDF;
-float fractFloat = 0.0000;
-int x;
-int valueInt = 0;
-int sign = 0;
-
-//Setting up interrupts High Interrupt
-#pragma code high_vector = 0x08
-void high_interrupt(void) 
-{
-    if (INTCON3bits.INT1F == 1)
-    {
-        _asm
-                goto LVDisr
-        _endasm
-    }
-    
-    if (INTCONbits.TMR0IF == 1)
-    {
-    _asm
-            goto timer10sIsr
-    _endasm
-    }
-    if (INTCONbits.INT0IF == 1)
-    {
-        _asm
-            goto bpmCountIsr
-        _endasm
-    }
-    
-    if (INTCON3bits.INT2IF == 1)
-    {
-    _asm
-            goto KeyPressInterrupt
-    _endasm
-    }
-}
-
-#pragma code
-
-#pragma interrupt KeyPressInterrupt
-void KeyPressInterrupt(void) 
-{
-        key = (BIT0 * 1 + BIT1 * 2 + BIT2 * 4 + BIT3 * 8);
-        switch (key) {
-            case (0):                       //Key Press 1 to start taking BPM reading
-                //heartRateModule();
-                break;
-            case (1):
-                //TestBeep();
-                break;
-            case (2):
-
-                break;
-            case (3):
-                heartRateModule();
-                break;
-            case (4):
-
-                break;
-            case (5):
-
-                break;
-            case (6):
-
-                break;
-            case (7):
-
-                break;
-            case (8):
-
-                break;
-            case (9):
-
-                break;
-            case (10):
-
-                break;
-            case (11):
-
-                break;
-            case (12):
-
-                break;
-            case (13):
-
-                break;
-            case (14):
-
-                break;
-            case (15):
-
-                break;
-            default:
-                while (BusyXLCD());
-                SetDDRamAddr(0x00);
-                while (BusyXLCD());
-                putrsXLCD("An Error!");
-                while (BusyXLCD());
-                break;
-        }
-
-       
-        INTCONbits.INT0IF = 0;   //Clear the external interrupt flag.
-        return;
-}
-
 void main(void) {
-    //---------------------CONFIGURATIONS----------------------------
-    InitLCD();
-    //TRISBbits.RB4 = 0;
-    //PORTBbits.RB4 = 0;
-    initSpeakerModule();
-    configBOR();
-    configKeypad();         //This Function also enables all interrupts
-    TRISBbits.RB3 = 0;      //Temprature congiuration
-    
-    //---------------------------------------------------------------
+    initLCD();
+    xlcdQueue = xQueueCreate(2, sizeof(struct lcdMessage));
 
 
-    while (1)
-    {
-        initSequence();                             //Initialization Step
-        writeByte(0xCC);                            //Command: Skip the ROM Check; since we're using 1 sensor
-        writeByte(0x44);                            //Command: Start a temp conversion
-        PORTBbits.RB3 = 1;                          //Strong pull up to provide current that internal capacitor can't provide
-		
-        Delay10KTCYx(80);                           //800ms (750ms is recommended conversion time))
-
-
-        PORTBbits.RB3 = 0; //Turn off strong pull-up
-
-                                                    //Data Collection Sequence
-        initSequence();
-        writeByte(0xCC);                            //Command: Skip ROM Check
-        writeByte(0xBE);                            //Command: Read Scratch-pad
-        tempLSB = readByte();                       //Read in the temp data
-        tempMSB = readByte();
-                                                    //The integer value is the 4 lsb of the high reg
-                                                    //and the 4 msb of the low reg
-        tempVarLSB = tempLSB >> 4;
-        tempVarMSB = tempMSB << 4;
-        intTemp = tempVarMSB | tempVarLSB;
-        
-                                                    //The 4 lsb of the low reg contain the fraction
-                                                    //temperature reading
-        if (tempLSB & 0x01) {fractFloat += 0.0625;}
-        if (tempLSB & 0x02) {fractFloat += 0.125;}
-        if (tempLSB & 0x04) {fractFloat += 0.25;}
-        if (tempLSB & 0x08) {fractFloat += 0.5;}
-        fractInt = fractFloat * 1000;
-
-        sign = ((tempMSB >> 3)& 0x3F);
-        if (sign == 0) {
-            sprintf(dispVal,"+%d.%03d%cC",intTemp,fractInt,degChar);
-            intTemp = 0;
-            fractFloat = 0.0000;
-            fractInt = 0;
-        } else {
-            sprintf(dispVal,"-%d.%03d%cC",intTemp,fractInt,degChar);
-            intTemp = 0;
-            fractFloat = 0.0000;
-            fractInt = 0;
-        }
-        
-
-
+    if ((xTaskCreate(tempTask, "TempTask", (configMINIMAL_STACK_SIZE), NULL, 1, NULL)) != pdPASS) {
         while (BusyXLCD());
-        SetDDRamAddr(0x15);
+        SetDDRamAddr(0x00);
         while (BusyXLCD());
-        putsXLCD(dispVal);
+        putrsXLCD("TempFailed");
         while (BusyXLCD());
-
     }
 
-
-}
-
-
-//TEMPRATURE FUNCTIONS
-
-unsigned char initSequence(void) {
-    short dsFound = 0;
-    OW_TRIS = INPUT;                                //Line High
-    OW_LAT = 0;                                     //Line Low
-    OW_TRIS = OUTPUT;
-    Delay10TCYx(48);                                //Hold line low for 480us
-    OW_TRIS = INPUT;                                //Line High
-    Delay10TCYx(24);                                //Wait for presense pulse from ds1822
-    dsFound = !OW_PIN;
-    return dsFound;
-}
-
-void writeByte(unsigned char data) {
-    char i;
-    for (i = 0; i < 8; i++) {
-        OW_LAT = 0;                                 //Line Low
-        OW_TRIS = OUTPUT;
-        Delay10TCYx(1);                             //Start a write slot
-        OW_TRIS = data & 0x01;                      //if a WRITE0 keep low, if a WRITE1 make input
-        Delay10TCYx(5);
-        OW_TRIS = INPUT;                            //Line High
-        Nop();                                      //Wait 2us between writes
-        Nop();
-        data = data >> 1;                           //right shift the data and continue
+    if ((xTaskCreate(lcdTask, "LCDTask", (configMINIMAL_STACK_SIZE), NULL, 3, NULL)) != pdPASS) {
+        while (BusyXLCD());
+        SetDDRamAddr(0x40);
+        while (BusyXLCD());
+        putrsXLCD("LCDTaskFailed");
+        while (BusyXLCD());
     }
+    vTaskStartScheduler();
 }
 
-char readByte(void) {
-    char data = 0;
-    char i;
-    for (i = 0; i < 8; i++) {
-        OW_LAT = 0;
-        OW_TRIS = OUTPUT;                           //Line low to start Read Slot
-        Nop();                                      //Keep low for at least 2us
-        Nop();
-        OW_TRIS = INPUT;                            //Line high
-        Nop();                                      //wait for response
-        Nop();
-        Nop();
-        Nop();
-        Nop();
-        Nop();
-        Nop();
-        Nop();
-        data = (OW_PIN << i) | data;                //Read the bit and keep shifting it left
-        Delay10TCYx(5);                             //Complete the READ cycle
-    }
-    return data;
-}

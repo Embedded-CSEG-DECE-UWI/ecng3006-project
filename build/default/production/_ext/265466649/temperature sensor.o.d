cf4e8f8b76d2bffb3fa55fa32e23e9522afe7812
riyaadmohammed <riyaad.mohammed@my.uwi.edu>
2019-11-29 15:49:34 -0400

    Add files via upload

---
+++ build/default/production/_ext/265466649/temperature sensor.o.d
@@ -0,0 +1,5 @@
+build/default/production/_ext/265466649/temperature\ sensor.o:  \
+	C:/Users/riyaa/Downloads/L02_816008425/temperature\ sensor.c  \
+	C:/Users/riyaa/Downloads/L02_816008425/ow.h  \
+	C:/Users/riyaa/Downloads/L02_816008425/xlcd.h  \
+
